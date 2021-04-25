c5575866cb1d9527c132d2d1666af32817a2e19a
Franklin Abreu <abreufranklin589@gmail.com>
2021-04-24 21:58:33 -0400

    rework both files to work as cogs

--- bot_commands/quotes.py
+++ bot_commands/quotes.py
@@ -1,17 +1,33 @@
 import requests
 import json
+from discord.ext import commands
 
 
-class Quotes():
+class Quotes(commands.Cog):
 
-    def today(self):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def today(self, ctx):
+        """Sends today's quote."""
+
         response = requests.get("https://zenquotes.io/api/today")
         json_data = json.loads(response.text)
         quote = f"{json_data[0]['q']} - **{json_data[0]['a']}**"
-        return quote
+        await ctx.send(quote)
+        print("Today's quote sent.")
 
-    def random(self):
+    @commands.command()
+    async def random(self, ctx):
+        """Sends a random quote."""
+
         response = requests.get("https://zenquotes.io/api/random")
         json_data = json.loads(response.text)
         quote = f"{json_data[0]['q']} - **{json_data[0]['a']}**"
-        return quote
+        await ctx.send(quote)
+        print("Random quote sent.")
+
+
+def setup(bot):
+    bot.add_cog(Quotes(bot))
--- bot_commands/word_of_the_day.py
+++ bot_commands/word_of_the_day.py
@@ -1,11 +1,21 @@
 import datetime
+import discord
+import random
+from discord.ext import commands
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
-class WordOfTheDay():
+class WordOfTheDay(commands.Cog):
+    """word of the day commands and setup"""
 
-    def scrape():
+    colors = (0x00ffff, 0x9fe2bf, 0xccccff, 0xdfff00,
+              0xf08080, 0xeb984e, 0xff8b3d, 0xffaf7a)
+
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    def scrape(self):
         currentday = datetime.date.today().strftime("%B %d, %Y")
 
         url = "https://www.dictionary.com/e/word-of-the-day/"
@@ -51,3 +61,45 @@ class WordOfTheDay():
         example = exampleBase.replace(word, f"**{word}**")
 
         return (word, currentday, pronounciation, wordType, definition, example)
+
+    def wotd(self):
+        color = random.choice(self.colors)
+
+        (word, currentday, pronounciation,
+         wordType, definition, example) = self.scrape()
+
+        wordofDay = discord.Embed(
+            title=f"{word} | {currentday}", color=color)
+        wordofDay.set_footer(text="Word of the Day")
+        wordofDay.add_field(
+            name="Pronounciation", value=pronounciation, inline=False)
+        wordofDay.add_field(
+            name="Word type", value=f"*{wordType}*", inline=False)
+        wordofDay.add_field(
+            name="Definition", value=definition, inline=False)
+        wordofDay.add_field(
+            name="Example", value=example, inline=False)
+        return wordofDay
+
+    @commands.command()
+    async def word(self, ctx):
+        """Send word of the day."""
+
+        _word = self.wotd()
+        await ctx.send(embed=_word)
+        print("word sent")
+
+    @commands.command()
+    async def setup(self, ctx, _channel, _time="08:00"):
+        """Setup Word of the Day channel
+        and the time in which it gets posted."""
+
+        chan = discord.utils.get(ctx.guild.text_channels, name=_channel)
+        if chan == None:
+            await ctx.send(f"The channel given does not exist. Try to send a valid channel.")
+        else:
+            await ctx.send(f"Word of the day will be sent to <#{chan.id}> at {_time}")
+
+
+def setup(bot):
+    bot.add_cog(WordOfTheDay(bot))
