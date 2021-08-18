0335e9634f404e4a8605bc5dffecec1d0427ff7d
Franklin Abreu <abreufranklin589@gmail.com>
2021-08-17 22:43:30 -0400

    set groundwork for logging of voice chat participation

--- cogs/vc.py
+++ cogs/vc.py
@@ -1,6 +1,9 @@
 import os
-from configparser import ConfigParser
 import discord
+import pymongo
+import pytz
+from configparser import ConfigParser
+from datetime import datetime
 from discord.ext import commands, tasks
 
 
@@ -15,11 +18,16 @@ class VoiceChannel(commands.Cog):
         self.guild_id = int(self.config["DEFAULT"]["GUILD_ID"])
         self.vc_id = int(self.config["DEFAULT"]["VC_CATEGORY_ID"])
 
+        self.mongo = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.db = self.mongo["discord-bot"]
+        self.collection = self.db["vc-stats"]
+
     @commands.Cog.listener()
     async def on_ready(self):
         await self.bot.wait_until_ready()
-        self.guild = self.bot.get_guild(self.guild_id)
-        self.category = self.bot.get_channel(self.vc_id)
+        self.guild: discord.Guild = self.bot.get_guild(self.guild_id)
+        self.category: discord.CategoryChannel = self.bot.get_channel(
+            self.vc_id)
         print(self.category)
 
     @commands.command(name="vc")
@@ -28,9 +36,32 @@ class VoiceChannel(commands.Cog):
         for vc in self.category.voice_channels:
             ch_count = len(vc.members)
             if ch_count != 0:
-                print(f"name: {vc.name} members: {ch_count}")
+                print(f"name: {vc.name} members count: {ch_count}")
             total_member_count += ch_count
         print(f"total peeps in vc: {total_member_count}")
+
+        # channel_data = {}
+        # for vc in self.category.voice_channels:
+        #     channel_data[vc.name] = len(vc.members)
+        #     ch_count = len(vc.members)
+        #     total_member_count += ch_count
+        # print(channel_data)
+
+    @tasks.loop(hours=1)
+    async def vc_logs(self):
+        eastern = pytz.timezone("US/Eastern")
+        total_member_count = 0
+        channel_data = {}
+
+        for vc in self.category.voice_channels:
+            channel_data[vc.name] = len(vc.members)
+            ch_count = len(vc.members)
+            total_member_count += ch_count
+
+        payload = {
+            "time": datetime.now(eastern),
+            "member_count": total_member_count
+        }
 
 
 def setup(bot: commands.Bot):
